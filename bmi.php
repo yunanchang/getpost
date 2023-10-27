cbe45c6eee8915c8db2d126b8f4c0cb49eef1fa0
yunanchang <asdqscesz70731@yahoo.com.tw>
2023-10-27 09:54:03 +0800

    1027

---
+++ bmi.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>計算bmi</title>
+</head>
+<body>
+    <p>計算bmi</p>
+</body>
+</html>
+<?php
+header("location:index.php")
+?>
